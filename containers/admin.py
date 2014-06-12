435ce407f0d593cc839b955727f30934d69b1b27
John Davidge <jodavidg@cisco.com>
2014-06-12 15:36:55 +0100

    Implemented Django admin functions - primarily for testing purposes.

--- containers/admin.py
+++ containers/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from containers.models import Container, Router, Network, Vm, EmbeddedContainer, Endpoint
 
-# Register your models here.
+class RouterInLine(admin.StackedInline):
+	model = Router
+	extra = 1
+
+class NetworkInLine(admin.StackedInline):
+	model = Network
+	extra = 1
+
+class VmInLine(admin.StackedInline):
+	model = Vm
+	extra = 1
+
+class EmbeddedContainerInLine(admin.StackedInline):
+	model = EmbeddedContainer
+	extra = 1
+
+class EndpointInLine(admin.StackedInline):
+	model = Endpoint
+	extra = 1
+
+class ContainerAdmin(admin.ModelAdmin):
+	fields = ['name']
+	inlines = [RouterInLine,NetworkInLine,VmInLine,EmbeddedContainerInLine,EndpointInLine]
+
+admin.site.register(Container, ContainerAdmin)
--- containers/models.py
+++ containers/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 class Container(models.Model):
 	name = models.CharField(max_length=200)
+	def __unicode__(self):
+		return self.name
 
 class Router(models.Model):
 	container = models.ForeignKey(Container)
 	name = models.CharField(max_length=200)
 	temp_id = models.CharField(max_length=200)
 	endpoint = models.BooleanField(default=False)
+	def __unicode__(self):
+		return self.name
 
 class Vm(models.Model):
 	container = models.ForeignKey(Container)
@@ -17,6 +21,8 @@ class Vm(models.Model):
 	image_id = models.CharField(max_length=200)
 	flavor = models.CharField(max_length=200)
 	endpoint = models.BooleanField(default=False)
+	def __unicode__(self):
+		return self.name
 
 class Network(models.Model):
 	container = models.ForeignKey(Container)
@@ -26,12 +32,19 @@ class Network(models.Model):
 	temp_id = models.CharField(max_length=200)
 	cidr = models.CharField(max_length=200)
 	endpoint = models.BooleanField(default=False)
+	def __unicode__(self):
+		return self.name
 
 class EmbeddedContainer(models.Model):
 	container = models.ForeignKey(Container)
 	temp_id = models.CharField(max_length=200)
+	def __unicode__(self):
+		return self.id
 
 class Endpoint(models.Model):
+	container = models.ForeignKey(Container)
 	embedded_container = models.ForeignKey(EmbeddedContainer)
 	endpoint_id = models.CharField(max_length=200)
 	connected_id = models.CharField(max_length=200)
+	def __unicode__(self):
+		return self.id
